eef2f12eb3bb176b7bd79891631cf4b33f0b3499
Robert <robert.crager18@gmail.com>
2021-01-24 01:39:05 -0600

    Update and finish program.

--- PSET2/PGM1.py
+++ PSET2/PGM1.py
@@ -1,4 +1,24 @@
 # Sales Tax Program
 # Variables used for tax calculation
 countyTax = 0.02
-saleTax = 0.04
+stateTax = 0.04
+
+# Ask for pruchase total before taxes.
+subtotal = float(input("What is the total? "))
+
+# Totals with taxes
+stateTaxTotal = subtotal*stateTax
+countyTaxTotal = subtotal*countyTax
+total = subtotal + (subtotal*(countyTax+stateTax))
+
+print(f"""
+========================================
+Subtotal:           {subtotal:.2f}
+----------------------------------------
+State Tax:          +{stateTaxTotal:.2f}
+County Tax:         +{countyTaxTotal:.2f}
+Total Sales Tax:    +{stateTaxTotal+countyTaxTotal:.2f}
+----------------------------------------
+Total:              {total:.2f}
+========================================
+)""")
